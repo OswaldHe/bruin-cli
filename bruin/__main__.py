aa8ae30b009168a4af80b00c6fb240d1445ec528
OswaldHe <zifanhe1202@ucla.edu>
2021-09-05 16:17:13 -0700

    finish mvp for meal

--- bruin/__main__.py
+++ bruin/__main__.py
@@ -1,5 +1,5 @@
 import argparse
-from bruin.meal import print_menu_all
+from bruin.meal import print_menu_all, print_hour, print_menu_detail_all, Period
 
 parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 
@@ -8,10 +8,37 @@ parser.add_argument("option", type=str, help=
 meal: print today's menu for each dining hall."
 )
 
+parser.add_argument(
+    "--hour", 
+    type=str, 
+    dest='hour', 
+    action='store', 
+    default=None, 
+    help='Used for meal: fetch hour of operation for dining halls [=all, =\'De Neve\', etc.]'
+)
+
+parser.add_argument(
+    "--detail", 
+    type=str, 
+    dest='detail', 
+    action='store', 
+    default=None, 
+    help='Print detail menu for [=Breakfast, =Lunch, =Dinner]'
+)
+
 def main():
     args = parser.parse_args()
     if args.option == "meal":
-        print_menu_all()
+        if args.hour is not None:
+            print_hour(args.hour)
+        elif args.detail is not None:
+            if Period.contains(args.detail):
+                print_menu_detail_all(args.detail)
+            else:
+                print("Invalid argument for --detail:")
+                parser.print_help()
+        else:
+            print_menu_all()
     else:
         parser.print_help()
 
