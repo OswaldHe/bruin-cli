f988b36f63388da5ddd83860808e76418297156d
OswaldHe <zifanhe1202@ucla.edu>
2021-09-05 17:26:53 -0700

    change readme

--- setup.py
+++ setup.py
@@ -12,17 +12,21 @@ with open(path.join(CURDIRT, 'requirements.txt'), encoding='utf-8') as f:
     all_reqs = f.read().split('\n')
 
 install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (not x.startswith("#"))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if 'git+' not in x]
 
 setup(
     name='bruin',
     description="Command line tool made by python for various toolbox used in UCLA",
-    version="0.1.2",
+    version="0.1.4",
     packages=find_packages(),
+    install_requires=install_requires,
     python_requires='>=3.6',
     entry_points='''
         [console_scripts]
         bruin=bruin.__main__:main
     ''',
+    dependency_links=dependency_links,
+    license='MIT',
     author='Oswald He',
     long_description=README,
     long_description_content_type="text/markdown",
